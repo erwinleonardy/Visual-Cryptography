9b4f0da5d4ecefbcd310d0cff7dcd1373164c80f
erwinleonardy <erwin.leonardy@yahoo.com>
2019-04-02 15:25:46 +0800

    Updated automated email content

--- emailer.py
+++ emailer.py
@@ -15,15 +15,28 @@ def emailer (receiver_email, filename):
 	message["Subject"] = "Your VSignIt Share"
 	message["From"] = sender_email
 	message["To"] = receiver_email
+	username = receiver_email.split("@")[0]
 
 	# create mail here
 
 	text = """\
-	Hi,
+	Hi, """ + username
 
-	Thank You for Using VSignIt!
+	text += """!\
 	
-	Here is your share!"""
+
+	Here is your share!
+	
+	Thank you for choosing VSingIt as your preferred encryption method.
+
+	(This is an auto-generated email. Please do not reply directly to this email.)
+
+	***** DISCLAIMER *****
+	This email and any attachments thereto are intended for the sole use of the recipient(s)  named above and 
+	may contain information that is confidential and/or proprietary to the VSingIt Group. Any use of the information 
+	contained herein (including, but not limited to, total or partial reproduction, communication, or dissemination 
+	in any form) by persons other than the intended recipient(s) is prohibited. If you have received this email in error, 
+	please notify the sender immediately and delete it."""
 
 
 	part1 = MIMEText(text, "plain")
