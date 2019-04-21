08a780f83189fd4e42d771c6cce964bd7c4aa860
Harpreet Kang <hpkang09@gmail.com>
2019-04-22 06:49:29 +0800

    (Emailer Service) Fixing indentations Changing 2 space indentation to 4 space for consistency

--- vsignit/emailerService.py
+++ vsignit/emailerService.py
@@ -4,46 +4,46 @@ from threading import Thread
 
 class EmailerService:
     @staticmethod
-	def async_email(msg):
-		with app.app_context():
-			mail.send(msg)
+    def async_email(msg):
+        with app.app_context():
+            mail.send(msg)
 	
-	@staticmethod
-	def send_email(username, receiver_email, subject, msg_body):
-		body =  """\
-		Hi, {}!\n""".format(username)
-		body += msg_body
-		body += """
+    @staticmethod
+    def send_email(username, receiver_email, subject, msg_body):
+        body =  """\
+        Hi, {}!\n""".format(username)
+        body += msg_body
+        body += """
         
         Thank you for choosing VSignIt as your preferred encryption method.
         
-		(This is an auto-generated email. Please do not reply directly to this email.)
+        (This is an auto-generated email. Please do not reply directly to this email.)
         
-		***** DISCLAIMER *****
-		This email and any attachments thereto are intended for the sole use of the recipient(s) named above and 
-		may contain information that is confidential and/or proprietary to the VSingIt Group. If you have received 
-		this email in error, please notify the sender immediately and delete it.
+        ***** DISCLAIMER *****
+        This email and any attachments thereto are intended for the sole use of the recipient(s) named above and 
+        may contain information that is confidential and/or proprietary to the VSingIt Group. If you have received 
+        this email in error, please notify the sender immediately and delete it.
         
-		Best Regards,
-		VSignIt Admin Team"""
+        Best Regards,
+        VSignIt Admin Team"""
 
-		msg = Message(subject=subject, body=body, recipients=[receiver_email])
-		thread = Thread(target=EmailerService.async_email, args=[msg])
-		thread.start()
+        msg = Message(subject=subject, body=body, recipients=[receiver_email])
+        thread = Thread(target=EmailerService.async_email, args=[msg])
+        thread.start()
 
 
-		# text += """!\
+        # text += """!\
 		
-		# Here is your share! Please ensure that you keep this file safe :)
+        # Here is your share! Please ensure that you keep this file safe :)
 		
-		# Thank you for choosing VSignIt as your preferred encryption method.
+        # Thank you for choosing VSignIt as your preferred encryption method.
 
-		# (This is an auto-generated email. Please do not reply directly to this email.)
+        # (This is an auto-generated email. Please do not reply directly to this email.)
 
-		# ***** DISCLAIMER *****
-		# This email and any attachments thereto are intended for the sole use of the recipient(s) named above and 
-		# may contain information that is confidential and/or proprietary to the VSingIt Group. If you have received 
-		# this email in error, please notify the sender immediately and delete it.
+        # ***** DISCLAIMER *****
+        # This email and any attachments thereto are intended for the sole use of the recipient(s) named above and 
+        # may contain information that is confidential and/or proprietary to the VSingIt Group. If you have received 
+        # this email in error, please notify the sender immediately and delete it.
 		
-		# Best Regards,
-		# VSignIt Admin Team"""
+        # Best Regards,
+        # VSignIt Admin Team"""
