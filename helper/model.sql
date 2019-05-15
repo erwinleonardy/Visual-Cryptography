cd2726bc21a4c69c0e9aae5fd4cc38e97045a95e
erwinleonardy <erwin.leonardy30@gmail.com>
2019-05-15 17:32:31 +0800

    (Helper files) Updated SQL Dump file

--- helper/model.sql
+++ helper/model.sql
@@ -21,6 +21,7 @@ SET time_zone = "+08:00";
 --
 -- Database: `vsignit`
 --
+CREATE DATABASE IF NOT EXISTS `vsignit`
 
 -- --------------------------------------------------------
 
@@ -38,18 +39,6 @@ CREATE TABLE IF NOT EXISTS `bank_data` (
   `bank_share_path` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `bank_data`
---
-
-INSERT INTO `bank_data` (`bank_user_id`, `client_user_id`, `bank_share_path`) VALUES
-(1, 2, './vsignit/output/bank/janedoe_DBS_bank_share.png'),
-(1, 3, './vsignit/output/bank/johndoe_DBS_bank_share.png'),
-(1, 4, './vsignit/output/bank/timapple_DBS_bank_share.png'),
-(1, 7, './vsignit/output/bank/donaldtrump_DBS_bank_share.png'),
-(1, 8, './vsignit/output/bank/matthewyeo_DBS_bank_share.png'),
-(1, 9, './vsignit/output/bank/mohammedali_DBS_bank_share.png'),
-(1, 10, './vsignit/output/bank/kimjongun_DBS_bank_share.png');
 
 --
 -- Indexes for dumped tables
@@ -78,19 +67,6 @@ CREATE TABLE IF NOT EXISTS `client_data` (
   `bank_user_id` int(11) NOT NULL,
   `client_share_path` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
-
---
--- Dumping data for table `client_data`
---
-
-INSERT INTO `client_data` (`client_user_id`, `bank_user_id`, `client_share_path`) VALUES
-(2, 1, './vsignit/output/client/janedoe_DBS_client_share.png'),
-(3, 1, './vsignit/output/client/johndoe_DBS_client_share.png'),
-(4, 1, './vsignit/output/client/timapple_DBS_client_share.png'),
-(7, 1, './vsignit/output/client/donaldtrump_DBS_client_share.png'),
-(8, 1, './vsignit/output/client/matthewyeo_DBS_client_share.png'),
-(9, 1, './vsignit/output/client/mohammedali_DBS_client_share.png'),
-(10, 1, './vsignit/output/client/kimjongun_DBS_client_share.png');
 
 --
 -- Indexes for dumped tables
@@ -122,16 +98,6 @@ CREATE TABLE IF NOT EXISTS `transaction` (
   `filepath` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
---
--- Dumping data for table `transaction`
---
-
-INSERT INTO `transaction` (`transaction_number`, `client_user_id`, `bank_user_id`, `timestamp`, `filepath`) VALUES
-('138b82ce5a4eb63ea14456a908cf0d779c601fe2', 8, 1, '2019-05-09 15:58:00', './vsignit/output/cheque/cheque_138b82ce5a4eb63ea14456a908cf0d779c601fe2.png'),
-('33a3c36314f7c5ff8d11fd188a0ec0418eb3ab4d', 2, 1, '2019-05-09 15:57:29', './vsignit/output/cheque/cheque_33a3c36314f7c5ff8d11fd188a0ec0418eb3ab4d.png'),
-('3f41b1f7ea3f1c4e3488019aef5f40fcd2a844b2', 9, 1, '2019-05-09 15:58:21', './vsignit/output/cheque/cheque_3f41b1f7ea3f1c4e3488019aef5f40fcd2a844b2.png'),
-('9ec1dfa2a24f6e34d05ef3dec1016f8bb07bb12b', 3, 1, '2019-05-09 15:57:42', './vsignit/output/cheque/cheque_9ec1dfa2a24f6e34d05ef3dec1016f8bb07bb12b.png'),
-('da3cf4bc07b8056ffbe34e50c29fc67c78fc7b6a', 3, 1, '2019-05-09 15:57:17', './vsignit/output/cheque/cheque_da3cf4bc07b8056ffbe34e50c29fc67c78fc7b6a.png');
 --
 -- Indexes for dumped tables
 --
@@ -168,13 +134,7 @@ CREATE TABLE IF NOT EXISTS `user_table` (
 INSERT INTO `user_table` (`user_id`, `user_type`, `email`, `username`, `password`) VALUES
 (1, 'admin', 'gafusoyici@stattech.info', 'DBS', 'pbkdf2:sha256:150000$3DaWGCwB$90db741fbaa5420fad5cee64646562e74d355d553e09488adc881a0ca4babae1'),
 (2, 'user', 'gafusoyici@stattech.info', 'janedoe', 'pbkdf2:sha256:150000$kouFGPTV$f7890d5acbb3ec18c83f3af4fbe6cfc1daa21d7976986dc8d04879dd4f91c237'),
-(3, 'user', 'gafusoyici@stattech.info', 'johndoe', 'pbkdf2:sha256:150000$8SdvwIgg$76228fa362efeddcc71e20744d44b69002cc29b2263e3c232772c8241d03bc9b'),
-(4, 'user', 'gafusoyici@stattech.info', 'timapple', 'pbkdf2:sha256:150000$7mOYt08J$fc0307caf5bdb16f1933c1e4244b9d27fc7cb5c3cca54d46639283f9ef08cc37'),
-(5, 'admin', 'gafusoyici@stattech.info', 'HSBC', 'pbkdf2:sha256:150000$Dj5kYxxr$fb25ef376b2d2ff36269e9dcd8eaed15f80dedf884345f146abea7421c306480'),
-(7, 'user', 'gafusoyici@stattech.info', 'donaldtrump', 'pbkdf2:sha256:150000$uloLEq3t$5fc7cc6cfbc624f8e1602e1e3e18488c254d42328c1b615eedc265cdbd510010'),
-(8, 'user', 'gafusoyici@stattech.info', 'matthewyeo', 'pbkdf2:sha256:150000$V9YwLwB4$158c7d584b6b13cb807ae1bf0a92eae9de5f2d88ee5b2548027a696a9b5b88b1'),
-(9, 'user', 'gafusoyici@stattech.info', 'mohammedali', 'pbkdf2:sha256:150000$31Af1YXM$dd8d06b2b29988b7f5fc8bb004c1610f4f9346c641da0ebda5ad443a64eeb2fe'),
-(10, 'user', 'gafusoyici@stattech.info', 'kimjongun', 'pbkdf2:sha256:150000$G2P54b7J$92b3850bc46014d1d84ecf38599b07b3ac2d1f4136e6f145d7a6ca213dae4182');
+(3, 'user', 'gafusoyici@stattech.info', 'johndoe', 'pbkdf2:sha256:150000$8SdvwIgg$76228fa362efeddcc71e20744d44b69002cc29b2263e3c232772c8241d03bc9b');
 
 --
 -- Indexes for dumped tables
@@ -195,5 +155,5 @@ ALTER TABLE `user_table`
 -- AUTO_INCREMENT for table `user_table`
 --
 ALTER TABLE `user_table`
-  MODIFY `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
