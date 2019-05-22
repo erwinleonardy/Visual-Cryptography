256c038d3119d3ef66b8d9dc2bb9cd9c14c545b1
erwinleonardy <erwin.leonardy30@gmail.com>
2019-05-22 15:50:39 +0800

    (Register) Added Password Strength Checker

--- vsignit/register.py
+++ vsignit/register.py
@@ -2,6 +2,7 @@
 # Author: Erwin Leonardy
 # Descrption: This file consists of the methods to sign up and redirect users to the login page
 
+import re
 from werkzeug.security import generate_password_hash
 
 from vsignit.models import UserType, User
@@ -18,6 +19,34 @@ class Register():
   # Function registers the users if and only if the username
   # doesn't exist and both of the password match
   def register(self):
+    # Verify the strength of 'password'
+    # A password is considered strong if:
+    #     8 characters length or more
+    #     1 digit or more
+    #     1 symbol or more
+    #     1 uppercase letter or more
+    #     1 lowercase letter or more
+
+    # calculating the length
+    if len(self.password) < 8:
+      return "Your password should have at least 8 characters!"
+
+    # searching for digits
+    elif re.search(r"\d", self.password) is None:
+      return "Your password should have at least a digit or more!"
+
+    # searching for uppercase
+    elif re.search(r"[A-Z]", self.password) is None:
+      return "Your password should have at least one uppercase letter!"
+
+    # searching for lowercase
+    elif re.search(r"[a-z]", self.password) is None:
+      return "Your password should have at least one lowercase letter!"
+
+    # searching for symbols
+    elif re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', self.password) is None:
+      return "Your password should have at least one symbol!"
+
     # if the user failed to re-enter the same password
     # else if the password matches and the username doesn't exist
     if self.password != self.verification:
