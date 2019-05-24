1f89669cc9cc7decca54f7e9bcaeaba8d388a76f
erwinleonardy <erwin.leonardy30@gmail.com>
2019-05-24 11:11:25 +0800

    (ALL) Small Cheque Bug Fix - NEW

--- vsignit/client.py
+++ vsignit/client.py
@@ -36,15 +36,12 @@ class Client:
 
     self.cheque.save(cheque_path)
     self.cheque = Common.openImage(cheque_path)
-    self.cheque = self.cheque.convert("RGBA")
+    if self.cheque.mode == 'JPG':
+      self.cheque = self.cheque.convert("RGB")
+    else:
+      self.cheque = self.cheque.convert("RGBA")
     print("Format: {}, Mode: {}".format(self.cheque.format, self.cheque.mode))
 
-    # self.cheque.save(cheque_path, format="PNG")
-    # self.cheque = Common.openImage(cheque_path)
-    # self.cheque = self.cheque.convert("RGBA")
-    # print("Format: {}, Mode: {}".format(self.cheque.format, self.cheque.mode))
-
-    print("(After) Format: {}, Mode: {}".format(self.cheque.format, self.cheque.mode))
     chequeDBPath = "tmp/chequeNew.png"
     chequePath = "./vsignit/output/chequeNew.png"
     Common.save_image(self.cheque,chequePath)
